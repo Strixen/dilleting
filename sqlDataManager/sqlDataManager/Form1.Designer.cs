fd93098de16804df2a174ae855ac185f0a8ba42f
Stedd <tor.andre.paulsen@gmail.com>
2017-03-24 20:02:22 +0100

    zcxzxc

--- sqlDataManager/sqlDataManager/Form1.Designer.cs
+++ sqlDataManager/sqlDataManager/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(40, 59);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(260, 24);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Make More Magic";
+            this.button1.Text = "Make Less Magic";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
