66648ecf61aa1e90df7cfb172a6145dc2781c7a5
Stedd <tor.andre.paulsen@gmail.com>
2017-03-24 19:32:32 +0100

    No i want less magic...

--- sqlDataManager/sqlDataManager/Form1.Designer.cs
+++ sqlDataManager/sqlDataManager/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(40, 59);
+            this.button1.Location = new System.Drawing.Point(40, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(156, 51);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Make More Magic";
+            this.button1.Text = "Make Less Magic";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
